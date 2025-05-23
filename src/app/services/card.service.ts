458e0b07b1adf086d091191c5b0382d86a94aa29
Dmitry Prokopenko <6558822@gmail.com>
2025-05-23 15:05:24 +0200

    Translate card texts and update contact info

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -9,8 +9,8 @@ export class CardService {
     { title: 'Полный круг услуг', description: 'Проводим полный цикл разработки – от концепции и дизайна до тестирования и поддержки.', imageUrl: 'assets/imagies/card1.png' },
     { title: 'Инновационные решения', description: 'Мы создаем  решения, помогающие вашему бизнесу выделяться в цифровом пространстве.', imageUrl: 'assets/imagies/card2.png' },
     { title: 'Постоянная коммуникация', description: 'Регулярно информируем клиентов о ходе проекта и обеспечиваем открытое взаимодействие на всех этапах разработки.', imageUrl: 'assets/imagies/card3.png' },
-    { title: 'Гибкость и масштабируемость', description: 'Наши решения легко адаптируются в соответствии с ростом вашего бизнеса и изменяющимися требованиями рынка.', imageUrl: 'assets/imagies/card4.png' },
-    { title: 'Краткие сроки выполнения', description: 'Оптимизированные процессы разработки позволяют нам эффективно управлять временем и сдавать проекты точно в срок.', imageUrl: 'assets/imagies/card5.png' },
+    { title: 'Flexibility and scalability', description: 'Our solutions easily adapt to your business growth and changing market requirements.', imageUrl: 'assets/imagies/card4.png' },
+    { title: 'Short delivery times', description: 'Optimized development processes allow us to manage time efficiently and deliver projects on schedule.', imageUrl: 'assets/imagies/card5.png' },
   ];
 
   getCards() {
