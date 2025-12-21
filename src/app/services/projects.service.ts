0ac71e68c4105ce95183908206c169ca8a8e3d08
Akor Direct <6558822@gmail.com>
2025-12-21 21:15:19 +0100

    Update Insaturbo project URL

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -8,7 +8,7 @@ export class ProjectsService {
   projects = [
     { title: 'PROJECTS.LIST.1.NAME', description: 'PROJECTS.LIST.1.DESC', imageUrl: 'assets/imagies/project1.jpg', url: 'https://sofantastica.pl/' },
     { title: 'PROJECTS.LIST.2.NAME', description: 'PROJECTS.LIST.2.DESC', imageUrl: 'assets/imagies/project2.jpg', url: 'https://salivonchyk.pl/' },
-    { title: 'PROJECTS.LIST.3.NAME', description: 'PROJECTS.LIST.3.DESC', imageUrl: 'assets/imagies/project3.jpg', url: 'https://www.insaturbo4x4.com/' },
+    { title: 'PROJECTS.LIST.3.NAME', description: 'PROJECTS.LIST.3.DESC', imageUrl: 'assets/imagies/project3.jpg', url: 'https://www.insaturbo4x4.pl/' },
     { title: 'PROJECTS.LIST.4.NAME', description: 'PROJECTS.LIST.4.DESC', imageUrl: 'assets/imagies/project4.jpg', url: 'https://www.get-together.events/' },
     { title: 'PROJECTS.LIST.5.NAME', description: 'PROJECTS.LIST.5.DESC', imageUrl: 'assets/imagies/project5.jpg', url: 'https://english-katt.vercel.app/' },
     { title: 'PROJECTS.LIST.6.NAME', description: 'PROJECTS.LIST.6.DESC', imageUrl: 'assets/imagies/project6.jpg', url: '' },
